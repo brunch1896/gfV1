fab72c23c36ff084b7cbc1aed8f11f1607811407
brunch1896 <brunch_zhu@163.com>
2023-09-09 22:09:57 +0800

    Delete imageCreation.py

--- imageCreation.py
+++
@@ -1,9 +0,0 @@
-import openai
-
-response = openai.Image.create(
-  prompt="A fluffy white cat with blue eyes sitting in a basket of flowers, looking up adorably at the camera",
-  n=1,
-  size="1024x1024"
-)
-image_url = response['data'][0]['url']
-print(image_url)
